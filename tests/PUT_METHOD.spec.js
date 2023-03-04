4ad9b5ab7c7758f3b7c1f2df723dce06e1c39e00
thuynhvan <thuynhvan@dotdash.com>
2023-03-04 10:26:22 -0600

    lesson-4

---
+++ tests/PUT_METHOD.spec.js
@@ -0,0 +1,34 @@
+const {test, expect} = require('@playwright/test')
+
+test('', async ({request}) => {
+
+    // Construct data
+    const url = "https://jsonplaceholder.typicode.com/posts/1";
+    const postContent = {
+        id: 1,
+        title: 'Tui la Teo',
+        body: 'This is the body',
+        userId: 1,
+    }
+    const options = {
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+        data: postContent
+    };
+
+    // Send PUT request
+    const response = await request.put(url, options);
+    const status = response.status();
+    const bodyJson = await response.json();
+
+    // Verification
+    expect(status).toBe(200);
+    const {id, title, body, userId} = bodyJson;
+    expect(status).toBe(200);
+    expect(id).toBe(postContent.id);
+    expect(userId).toBe(postContent.userId);
+    expect(title).toBe(postContent.title);
+    expect(body).toBe(postContent.body);
+
+})
