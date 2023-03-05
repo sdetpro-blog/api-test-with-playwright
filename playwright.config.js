55d1a5b3e1c6d6befd0ca3fbc0bc8f4898a4512a
thuynhvan <thuynhvan@dotdash.com>
2023-03-05 15:44:12 -0600

    lesson-10

--- playwright.config.js
+++ playwright.config.js
@@ -34,6 +34,7 @@ const config = {
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
         ['list'],
+        ['./reporter/CustomReporter.js'],
         ['html', {
         open: 'always',
         port: 9999
---
+++ reporter/CustomReporter.js
@@ -0,0 +1,24 @@
+// my-awesome-reporter.js
+// @ts-check
+
+/** @implements {import('@playwright/test/reporter').Reporter} */
+class MyReporter {
+    onBegin(config, suite) {
+        // Send the data into kibana/grafana/3rd party in-house / other services
+        console.log(`Starting the run with ${suite.allTests().length} tests`);
+    }
+
+    onTestBegin(test) {
+        console.log(`Starting test ${test.title}`);
+    }
+
+    onTestEnd(test, result) {
+        console.log(`Finished test ${test.title}: ${result.status}`);
+    }
+
+    onEnd(result) {
+        console.log(`Finished the run: ${result.status}`);
+    }
+}
+
+module.exports = MyReporter;
